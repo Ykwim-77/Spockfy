d8f09e0b6480d237028c1f5add80531eeb222564
Christian <christian.darosa0106@gmail.com>
2025-08-19 22:30:05 -0300

    fim aula 10

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,7 +8,22 @@ export class SpotifyService{
 
     constructor(){}
     async obterUrlLogin(): Promise<string> {
+        const codigoAleatorio = await this.gerarCodigoAlatorio();
         const authPoint = `${SpotifyConfiguration.authEndpoint}?`;
+        const clientId = `cliente_id=${SpotifyConfiguration.clientId}&`;
+        const urlRedirect = `redirect_url=${SpotifyConfiguration.redirectUrl}&`;
+        const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}`;
+        const codeChallengeMethod = 'code_challenge_method=S256';
+        const codeChallengeParam = 'code_challenge' + codigoAleatorio + '&';
+        const responseType = 'response_type=code';
+        return `${authPoint}${clientId}${urlRedirect}${scopes}${codeChallengeMethod}${codeChallengeParam}${responseType}`
     }
+    async gerarCodigoAleatorio(){
+        const codigoVerificador = this.gerarCodigoVerificador(128);
+        const codChallenge = await this.gerarHash();
+    
+    return codChallenge
+    }
+
 
 }
