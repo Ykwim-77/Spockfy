3c373ce30aa3f8183e338f0cf84a5212fed79525
Christian <christian.darosa0106@gmail.com>
2025-09-16 19:59:13 -0300

    foi?

--- src/app/components/painel-esquerdo/painel-esquerdo.component.ts
+++ src/app/components/painel-esquerdo/painel-esquerdo.component.ts
@@ -1,7 +1,8 @@
-import {ChangeDetectionStrategy, Component, OnInit} from "@angular/core";
+import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from "@angular/core";
 import {BotaoMenuComponent} from '../botao-menu/botao-menu.component';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faHome, faSearch, faUser} from '@fortawesome/free-solid-svg-icons';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-painel-esquerdo',
@@ -20,7 +21,15 @@ export class PainelEsquerdoComponent implements OnInit {
   faSearch = faSearch;
   faUser = faUser;
 
-  ngOnInit(): void {
+  menuSelecionado: string = 'Inicio';
+  playlists = signal<SpotifyApi.PlaylistObjectSimplified[]>([]);
+  spotifyService = inject(SpotifyService);
 
+  ngOnInit(): void {
+    this.carregarPlaylists();
+  }
+
+  async carregarPlaylists() {
+    this.playlists.set(await this.spotifyService.carregarPlaylists());
   }
 }
